2cb063d4f4ecc846fd4a0d341e2ca68ff02b3d90
Fo4Ik <zinkovskyi.vladyslav@gmail.com>
2024-02-14 16:18:28 +0100

    create side menu

--- src/app/components/side-bar-menu/side-bar-menu.component.ts
+++ src/app/components/side-bar-menu/side-bar-menu.component.ts
@@ -1,26 +1,38 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AvatarModule} from "primeng/avatar";
 import {ConfigService} from "../../config/config.service";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-side-bar-menu',
   standalone: true,
   imports: [
-    AvatarModule
+    AvatarModule,
+    NgClass
   ],
   templateUrl: './side-bar-menu.component.html',
   styleUrl: './side-bar-menu.component.scss'
 })
-export class SideBarMenuComponent {
+export class SideBarMenuComponent implements OnInit {
   @Input() isMobile!: boolean;
   @Input() userProfilePhoto!: string;
   @Input() user!: any;
-
-  config: any = localStorage.getItem('config') || null;
+  isDarkTheme!: boolean; // Замените на логику определения темы
+  userConfig: any = JSON.parse(localStorage.getItem('userConfig') || '{}');
 
   constructor(private configService: ConfigService) {
 
-    this.configService.saveConfig(this.config)
   }
+
+  ngOnInit(): void {
+    this.userConfig.style.theme === 'dark' ? this.isDarkTheme = true : this.isDarkTheme = false;
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.userConfig.style.theme = this.isDarkTheme ? 'dark' : 'light';
+    this.configService.changeTheme()
+  }
+
 
 }
